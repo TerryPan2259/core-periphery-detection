7eefd5a75b222106cfb7e8705b0f4052c90e2751
Sadamori Kojaku <freesailing4046@gmail.com>
2018-07-04 11:19:28 +0100

    sa

---
+++ test.py
@@ -0,0 +1,23 @@
+import csv
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+from kmalgorithm import km_config, km_modmat
+
+def test1():
+	network = nx.karate_club_graph()
+	cppairs = km_config(network, significance_level = 0.05)
+
+	assert 'pair_id' in cppairs 
+	assert 'core_node' in cppairs 
+	assert 'cp_pair_significance' in cppairs 
+
+def test2():
+	network = nx.karate_club_graph()
+
+	cppairs = km_dense(network, significance_level = 0.05)
+
+	assert 'pair_id' in cppairs 
+	assert 'core_node' in cppairs 
+	assert 'cp_pair_significance' in cppairs 
