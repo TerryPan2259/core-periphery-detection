d50f5aa9afdbc0c7d9daa9acbd953b256fdde5d7
Sadamori Kojaku <freesailing4046@gmail.com>
2018-07-06 21:34:51 +0100

    sa

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to core-periphery detection in networks documentation!"],titleterms:{core:0,detect:0,document:0,indic:0,network:0,peripheri:0,tabl:0,welcom:0}})
